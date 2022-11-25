ccf9dea03d180d46dfdd6c6933ecc4179490b13d
Huy Phan <huy.pb.2011@gmail.com>
2022-11-25 11:52:04 +0700

    random-advice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState(1);
+
+  const fetchAdvice = async (id) => {
+    const data = await axios
+      .get(`https://api.adviceslip.com/advice/${id}`)
+      .then((response) => response.data.slip.advice)
+      .catch((error) => {
+        console.log(error);
+      });
+    setAdvice(data);
+    console.log("data", data);
+  };
+  // console.log(fetchAdvice(129));
+
+  const handleClick = () => {
+    const min = 1;
+    const max = 200;
+    const idRandom = min + Math.floor(Math.random() * (max - min));
+    console.log(idRandom);
+    setId(idRandom);
+  };
+  // const handleSetAdvice = () => {
+  //   setAdvice(fetchAdvice());
+  // };
+
+  // const { advice } = fetchAdvice();
+  // console.log(fetchAdvice);
+
+  useEffect(() => {
+    id && fetchAdvice(id);
+  }, [id]);
+
+  useEffect(() => {
+    fetchAdvice(id);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="random">
+        <div className="advice">
+          <span>{advice}</span>
+        </div>
+        <div className="button">
+          <button onClick={() => handleClick()}>RANDOM</button>
+        </div>
+      </div>
     </div>
   );
 }
